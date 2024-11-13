d6d4bc74f92d7048b52633c172d674219375075d
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-13 19:35:34 +0100

    show internationalisation with a swing component

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
+import javax.swing.*;
+import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Main {
   public static void main(String[] args) {
     
-    ResourceBundle rb = ResourceBundle.getBundle("BasicText");
-    System.out.println(rb.getClass().getName());
-    System.out.println(rb.getBaseBundleName());
-    System.out.println(rb.keySet());
+//    Locale.setDefault(Locale.CANADA_FRENCH);
+//    Locale.setDefault(Locale.of("en", "CA"));
+//    Locale.setDefault(Locale.US);
     
-    System.out.printf("%s %s!%n",
-      rb.getString("hello"), rb.getString("world"));
-    
+    for (Locale l : List.of(Locale.US, Locale.CANADA_FRENCH, Locale.CANADA)) {
+      
+      ResourceBundle rb = ResourceBundle.getBundle("BasicText", l);
+//      System.out.println(rb.getClass().getName());
+//      System.out.println(rb.getBaseBundleName());
+//      System.out.println(rb.keySet());
+      
+      String message = "%s %s!%n".formatted(
+        rb.getString("hello"), rb.getString("world"));
+      
+      ResourceBundle ui = ResourceBundle.getBundle("UIComponents", l);
+      
+      JOptionPane.showOptionDialog(null,
+        message,
+        ui.getString("first.title"),
+        JOptionPane.DEFAULT_OPTION,
+        JOptionPane.INFORMATION_MESSAGE,
+        null,
+//        new Object[]{ui.getString("btn.ok")}, // button label instead of "OK"
+        new Object[]{rb.getString("yes"), rb.getString("no")},
+        null);
+    }
   }
 }
